280cc53a78e819980c2e476248bbce5a2f8a4edb
Agustin <agustinlevental@gmail.com>
2024-10-17 14:13:47 -0300

    patch course

--- controllers/course.js
+++ controllers/course.js
@@ -34,8 +34,27 @@ function getCourse(req,res){
    
     }
 
+function updateCourse(req,res){
+    const {id} = req.params;
+    const courseData = req.body;
+
+    if(req.files.miniature){
+        const imagePath = image.getFilePath(req.files.miniature);
+    courseData.miniature=imagePath;
+    }
+
+    Course.findByIdAndUpdate({_id:id},courseData,(error)=>{
+if(error){
+    res.status(400).send({msg:"Error al actualizar el curso"})
+} else{
+    res.status(200).send({msg:"curso actualizado"})
+}
+    })
+}
+
 module.exports={
     createCourse,
-    getCourse
+    getCourse,
+    updateCourse
 };
 
