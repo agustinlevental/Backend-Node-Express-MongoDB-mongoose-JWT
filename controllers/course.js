6b6574d137228a1fdc6e9e3be4d29ebae0288be6
Agustin <agustinlevental@gmail.com>
2024-10-17 14:02:42 -0300

    course pagination schema

--- controllers/course.js
+++ controllers/course.js
@@ -18,10 +18,24 @@ if(error){
 })
 }
 
+function getCourse(req,res){
+    const {page=1,limit=10} = req.query;
+    const options = {
+        page:parseInt(page),
+        limit:parseInt(limit)
+    }
+    Course.paginate({},options,(error,courses)=>{
+        if(error){
+            res.status(400).send({msg:"Error al obtener los cursos"})
+        } else{
+            res.status(200).send(courses)
+        }
+    })
+   
+    }
+
 module.exports={
     createCourse,
+    getCourse
 };
 
-function getCourse(req,res){
-
-}
