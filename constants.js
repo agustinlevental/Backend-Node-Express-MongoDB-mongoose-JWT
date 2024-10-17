58a4c2606fd93e04c489ba29dd109186a323d356
Agustin <agustinlevental@gmail.com>
2024-10-17 16:51:44 -0300

    cambio de contraseña env

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const DB_USER = "agustinlevental";
-const DB_PASSWORD = process.env.DB_PASSWORD; 
 const DB_HOST = "mern.7ai8t.mongodb.net/?retryWrites=true&w=majority&appName=Mern";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
@@ -9,8 +8,7 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 module.exports = {
   DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
+    DB_HOST,
   API_VERSION,
   IP_SERVER,
   JWT_SECRET_KEY,
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose =  require("mongoose");
+require('dotenv').config();
 const app= require("./app")
-const {DB_USER,DB_PASSWORD,DB_HOST,IP_SERVER,API_VERSION} = require("./constants")
+const {DB_USER,DB_HOST,IP_SERVER,API_VERSION} = require("./constants")
+const DB_PASSWORD = process.env.DB_PASSWORD; 
+
 const PORT= process.env.port || "3977"
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`
