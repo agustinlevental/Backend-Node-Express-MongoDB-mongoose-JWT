66ed55310f3fd8e5188380ec7b0cf8b696c62579
Agustin <agustinlevental@gmail.com>
2024-10-17 15:19:37 -0300

    delete post

--- controllers/post.js
+++ controllers/post.js
@@ -51,7 +51,20 @@ function updatePost(req,res){
         }
     })
 }
+
+function deletePost(req,res){
+const {id} = req.params;
+
+Post.findByIdAndDelete({_id:id},(error)=>{
+    if(error){
+        res.status(400).send({msg:"Error al eliminar el post"})
+    } else{
+        res.status(200).send({msg:"Post eliminado"})
+    }
+})
+}
 module.exports={
     createPost,
     getPosts,
-    updatePost}
+    updatePost,
+    deletePost}
