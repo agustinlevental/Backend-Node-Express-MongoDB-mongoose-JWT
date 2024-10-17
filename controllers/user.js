ec52752947f27e5339e3862774aa2856d0ed6ec5
Agustin <agustinlevental@gmail.com>
2024-10-17 09:12:42 -0300

    get users with query

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,33 @@
+const User = require("../models/users")
+
 async function getMe(req,res){
-    res.status(200).send({msg:"ok"})
+    const {user_id} = req.user;
+
+    const responseUser = await User.findById(user_id);
+    
+    if(!responseUser){
+        res.status(400).send({msg:"no se ha encontrado usuario"})
+    } else{
+        res.status(200).send(responseUser)
+        
+    }
+    
+}
+
+async function getUsers(req,res){
+    const {active} = req.query;
+ let response =null 
+ if(active===undefined){
+    //devuelvo todos los usuarios
+    response= await User.find();
+ } else{
+    //devuelvo los activos o inactivos
+    response = await User.find({active})
+ }
+res.status(200).send(response)
 }
 
 module.exports={
-    getMe
+    getMe,
+    getUsers
 }
--- router/user.js
+++ router/user.js
@@ -4,4 +4,5 @@ const md_auth = require("../middlewares/authenticated")
 const api = express.Router();
 
 api.get("/user/me",[md_auth.asureAuth],userController.getMe);
+api.get("/users",[md_auth.asureAuth],userController.getUsers)
 module.exports=api;
